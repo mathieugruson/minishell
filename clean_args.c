8ba4fd43e50cbf36da749e8aa49590051e09c0e4
Mathieu GRUSON <mathieu.gruson@gmail.com>
2022-11-23 18:58:30 +0100

    no more 25l, line too long

--- clean_args.c
+++ clean_args.c
@@ -6,30 +6,30 @@
 /*   By: mgruson <mgruson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 19:10:48 by mgruson           #+#    #+#             */
-/*   Updated: 2022/11/23 18:23:53 by mgruson          ###   ########.fr       */
+/*   Updated: 2022/11/23 18:32:26 by mgruson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char *remove_quote(char *str, int quote, int a)
+char	*remove_quote(char *str, int quote, int a)
 {
 	t_index	i;
 
 	i = initialize_index();
 	a--;
-	while(str[a] != quote)
+	while (str[a] != quote)
 		a--;
 	i.i1 = a;
-	while(i.count < 2 && i.count++ > -1)
+	while (i.count < 2 && i.count++ > -1)
 	{
 		while (str[a])
 		{
 			if (str[a] == quote)
 			{	
-				while (str[a+1])
+				while (str[a + 1])
 				{
-					str[a] = str[a+1];
+					str[a] = str[a + 1];
 					a++;
 				}
 				str[a] = 0;
@@ -41,15 +41,15 @@ char *remove_quote(char *str, int quote, int a)
 	return (str);
 }
 
-int is_quote_in(char *str, int i)
+int	is_quote_in(char *str, int i)
 {
-	int j;
+	int	j;
 
 	j = 0;
 	while (str[i])
 	{
 		if (str[i] == 39 || str[i] == 34)
-			j++;		
+			j++;
 		i++;
 	}
 	if (j == 0)
@@ -57,23 +57,24 @@ int is_quote_in(char *str, int i)
 	return (1);
 }
 
-char *clear_quote(char *str)
+char	*clear_quote(char *str)
 {
-	t_index i;
-	
+	t_index	i;
+
 	i = initialize_index();
 	if (!is_quote_in(str, i.i))
 		return (str);
-	while(str[i.i])
+	while (str[i.i])
 	{
-		if (str[i.i] == 34 && i.j++ > 0 && i.j == 2 && (i.k == 2 || i.k == 0))	
+		if (str[i.i] == 34 && i.j++ > 0 && i.j == 2 && (i.k == 2 || i.k == 0))
 		{
 				str = remove_quote(str, 34, i.i);
 				i.j = 0;
 				i.k = 0;
 				i.i = i.i - 2;
 		}
-		else if (str[i.i] == 39 && i.k++ > 0 && i.k == 2 && (i.j == 2 || i.j == 0))
+		else if (str[i.i] == 39 && i.k++ > 0 \
+		&& i.k == 2 && (i.j == 2 || i.j == 0))
 		{
 				str = remove_quote(str, 39, i.i);
 				i.k = 0;
@@ -85,17 +86,17 @@ char *clear_quote(char *str)
 	return (str);
 }
 
-char ***clean_args(char ***cmd)
+char	***clean_args(char ***cmd)
 {
-	int i;
+	int	i;
 	int	j;
 
 	i = 0;
 	j = 0;
-	while(cmd[i])
+	while (cmd[i])
 	{
 		j = 0;
-		while(cmd[i][j])
+		while (cmd[i][j])
 		{
 			cmd[i][j] = clear_quote(cmd[i][j]);
 			j++;
