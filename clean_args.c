9ade1dbb5cb0256a45adb69c277b1f4c65046aac
Mathieu GRUSON <mathieu.gruson@gmail.com>
2022-12-06 17:21:42 +0100

    clean quote up

--- clean_args.c
+++ clean_args.c
@@ -6,7 +6,7 @@
 /*   By: mgruson <mgruson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 19:10:48 by mgruson           #+#    #+#             */
-/*   Updated: 2022/12/06 13:08:36 by mgruson          ###   ########.fr       */
+/*   Updated: 2022/12/06 17:20:29 by mgruson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,9 @@ char *remove_quote(char *str, int quote, int i)
 	{
 		if (str[i] == quote)
 		{	
-			while (str[i+1])
+			while (str[i + 1])
 			{
-				str[i] = str[i+1];
+				str[i] = str[i + 1];
 				i++;
 			}
 			str[i] = 0;
@@ -35,6 +35,7 @@ char *remove_quote(char *str, int quote, int i)
 		i++;
 	}
 	i = ibis;
+	printf("str 1: %s\n", str);
 	while (str[i])
 	{
 		if (str[i] == quote)
@@ -48,8 +49,18 @@ char *remove_quote(char *str, int quote, int i)
 		}
 		i++;
 	}
+	printf("str : %s\n", str);
 	return (str);
 }
+
+// int is_before(char *str, int i)
+// {
+// 	while(str[i] != 34 && str[i] != 39)
+// 		i--;
+// 	return ((int)str[i]);
+// }
+
+/* le -2 sert pour eviter que le i++ ligne 106 saute une quote*/
 
 char *clear_quote(char *str)
 {
@@ -69,26 +80,28 @@ char *clear_quote(char *str)
 	i = 0;
 	while(str[i])
 	{
-		if (str[i] == 34)	
+		if (str[i] == 34)
 		{
 			dq++;
-			if (dq == 2 && (sq == 2 || sq == 0))
+			if (dq == 2 && (sq == 2 || sq == 0 || !is_in_quote(str, i)))
 			{	
 				str = remove_quote(str, 34, i);
 				dq = 0;
 				sq = 0;
 				i = i - 2;
+				printf("c 1: %c, i : %i\n", str[i], i);
 			}
 		}
 		else if (str[i] == 39)
 		{
 			sq++;
-			if (sq == 2 && (dq == 2 || dq == 0))
+			if (sq == 2 && (dq == 2 || dq == 0 || !is_in_quote(str, i)))
 			{
 				str = remove_quote(str, 39, i);
 				sq = 0;
 				dq = 0;
 				i = i - 2;
+				printf("c : %c, i ; %i\n", str[i], i);
 			}
 		}
 		i++;
