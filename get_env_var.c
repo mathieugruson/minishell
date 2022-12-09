64d6d0fbcc4a8afbe9959d9119d624d106a5a7c0
Mathieu GRUSON <mathieu.gruson@gmail.com>
2022-12-09 22:19:41 +0100

    normi bug getenvvar.c

--- get_env_var.c
+++ get_env_var.c
@@ -6,7 +6,7 @@
 /*   By: mgruson <mgruson@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 16:31:04 by mgruson           #+#    #+#             */
-/*   Updated: 2022/12/09 22:14:41 by mgruson          ###   ########.fr       */
+/*   Updated: 2022/12/09 22:17:14 by mgruson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,18 +96,16 @@ char	*new_env_var(char *str, char **envp)
 			str = basic_env(str, envp, &i);
 		if (str[i.i] == '$' && str[i.i + 1] == '?' \
 		&& !is_in_simple_quote(str, i.i))
-		{
 			str = get_status(str, (i.i + ft_intlen(exit_status) + 1), \
 			(i.i + 1), ft_itoa(exit_status));
-		}
 		if (str[i.i] == '$' && (ft_isdigit(str[i.i + 1]) > 0 \
 		|| (str[i.i + 1] == 34 || str[i.i + 1] == 39)) \
 		&& !is_in_simple_quote(str, i.i))
 		{
-			if (ft_isdigit(str[i.i + 1]))	
+			if (ft_isdigit(str[i.i + 1]))
 				str = ft_strcpy(&str[i.i], &str[i.i + 2]);
 			else
-				str = ft_strcpy(&str[i.i], &str[i.i + 1]);					
+				str = ft_strcpy(&str[i.i], &str[i.i + 1]);
 			str = clear_quote(&str[i.i]);
 		}
 		i.i++;
